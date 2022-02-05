2f1d4a54311d07469e1a6c7ed65c873d4f41165b
VitorMurbach <vitormurbach626@gmail.com>
2022-02-05 12:51:49 -0300

    desenvolvimento projeto angular passo 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { RouterModule } from '@angular/router';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/Vagas.model.ts
@@ -0,0 +1,7 @@
+export class Vaga {
+    id: number = 0;
+    nome: string = "";
+    foto: string = "";
+    descricao: string = "";
+    salario: number = 0;
+}
